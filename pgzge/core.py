d196ef58dbb2165437bee5cd2e4e9ce0f7189aad
Daniel Bloy <daniel_bloy@hotmail.com>
2026-02-14 11:06:42 +0000

    More reworking of the core GameObject.

--- pgzge/core.py
+++ pgzge/core.py
@@ -9,7 +9,7 @@ class GameObject:
 
     A GameObject has the following properties:
         * name: The name of this object. Used when locating an object by name. Must not contain the `/`
-                or space characters.
+                or `.` characters.
         * active: This has to be True for the the GameObject to be updated or drawn (visible and
                   enabled also need to be True). The value of active is propagated to all children.
                   Changing active will also trigger the corresponding handlers.
@@ -25,19 +25,20 @@ class GameObject:
                   multiple parents.
         * children: A list of child GameObjects. These will be updated and drawn only if this
                     GameObject is active.
-        * draw_handlers: Draw handlers are called during draw() if the GameObject is active and visible.
-        * update_handlers: Update handlers are called during update() if the GameObject is active and enabled.
+        * draw_handlers: Draw handlers are called during `draw()` if the GameObject is active and visible.
+        * update_handlers: Update handlers are called during `update()` if the GameObject is active and enabled.
         * activate_handlers = Activate handlers are called when active changes from False to True.
         * deactivate_handlers = Deactivate handlers are called when active changes from True to False.
-        * destroyed_handlers = Destroy handlers are called when Destroy() is called on the GameObject.
+        * destroy_handlers = Destroy handlers are called when `destroy()` is called on the GameObject.
 
-        The update() and draw() methods propagate down the hierarchy if active is True and regardless of
-        visible and active which only apply to this GameObject instance). i.e. a child can be enabled and
+        The `update()` and `draw()` methods propagate down the hierarchy if active is True and regardless of
+        visible and active which only apply to this GameObject instance. i.e. a child can be enabled and
         visible even if the parent is not.
 
         Destroy, activate and deactivate are propagated to all children irrespective of whether active is
-        True or False. All handlers are called before passing to the children with the exception of
-        destroy which propagates first.
+        True or False. All handlers are called before passing to the children except for destroy which
+        propagates to the children first. In the case of activate and deactivate, the `activate()` and
+        `deactivate()` methods are called before any handlers.
 
         The handlers can be used to provide instance specific behaviour without having to make a subclass
         and override the relevant method.
@@ -53,12 +54,12 @@ class GameObject:
                  update_handler: Callable[[Self, float], None] = None,
                  activate_handler: Callable[[Self], None] = None,
                  deactivate_handler: Callable[[Self], None] = None,
-                 destroyed_handler: Callable[[Self], None] = None,
+                 destroy_handler: Callable[[Self], None] = None,
                  draw_handlers: list[Callable[[Self, Any], None]] = None,
                  update_handlers: list[Callable[[Self, float], None]] = None,
                  activate_handlers: list[Callable[[Self], None]] = None,
                  deactivate_handlers: list[Callable[[Self], None]] = None,
-                 destroyed_handlers: list[Callable[[Self], None]] = None):
+                 destroy_handlers: list[Callable[[Self], None]] = None):
         """
         Initialises a GameObjects properties with the provided arguments. All arguments are optional and
         have a corresponding property. The only point of note is that the active property is set twice to
@@ -78,13 +79,13 @@ class GameObject:
             Callable[[Self], None]] = activate_handlers.copy() if activate_handlers else []
         self.__deactivate_handlers: list[
             Callable[[Self], None]] = deactivate_handlers.copy() if deactivate_handlers else []
-        self.__destroyed_handlers: list[
-            Callable[[Self], None]] = destroyed_handlers.copy() if destroyed_handlers else []
+        self.__destroy_handlers: list[
+            Callable[[Self], None]] = destroy_handlers.copy() if destroy_handlers else []
         self.__draw_handlers.append(draw_handler) if draw_handler else None
         self.__update_handlers.append(update_handler) if update_handler else None
         self.__activate_handlers.append(activate_handler) if activate_handler else None
         self.__deactivate_handlers.append(deactivate_handler) if deactivate_handler else None
-        self.__destroyed_handlers.append(destroyed_handler) if destroyed_handler else None
+        self.__destroy_handlers.append(destroy_handler) if destroy_handler else None
         self.__destroyed: bool = False
         # This forces the active or deactivate events to be called.
         self.__active: bool = not active
@@ -94,7 +95,7 @@ class GameObject:
     def name(self) -> bool | None:
         """
         The name of this GameObject or None if the GameObject has no name. It should not contain the `/` or
-        space characters. It is used when locating an object by name.
+        `.`` characters. It is used when locating an object by name.
 
         :return: The name of this GameObject or None if the GameObject has no name.
         """
@@ -110,9 +111,9 @@ class GameObject:
     @active.setter
     def active(self, value: bool) -> None:
         """
-
-        :param value:
-        :return:
+        Setting active to True or False will activate or deactivate the object (only if the new active state
+        is different to the current active state). The active value is propagated to all children. In the
+        case where this object is `destroyed` then no action is taken.
         """
         # Cannot activate a destroyed GameObject.
         if self.__destroyed:
@@ -124,9 +125,11 @@ class GameObject:
 
         if do_handlers:
             if value:
+                self.activated()
                 for handler in self.__activate_handlers:
                     handler(self)
             else:
+                self.deactivated()
                 for handler in self.__deactivate_handlers:
                     handler(self)
 
@@ -134,10 +137,24 @@ class GameObject:
         for child in self.__children:
             child.active = value
 
+    def activated(self) -> Self:
+        """
+        This is called when the GameObject is activated. It provides an easy way for subclasses to provide
+        activation code without using handlers.
+        """
+        return self
+
+    def deactivated(self) -> Self:
+        """
+        This is called when the GameObject is deactivated. It provides an easy way for subclasses to provide
+        deactivation code without using handlers.
+        """
+        return self
+
     def activate(self) -> Self:
         """
         Activates the GameObject. This will trigger the activate handlers and propagate the active state to
-        all children.
+        all children. This is a shorthand version of `self.active = True` but also returns the GameObject.
         """
         self.active = True
         return self
@@ -145,7 +162,7 @@ class GameObject:
     def deactivate(self) -> Self:
         """
         Deactivates the GameObject. This will trigger the deactivate handlers and propagate the active state to
-        all children.
+        all children. This is a shorthand version of `self.active = False` but also returns the GameObject.
         """
         self.active = False
         return self
@@ -159,7 +176,8 @@ class GameObject:
 
     def destroy(self) -> None:
         """
-        Destroy is propagated to all children before the handlers are triggered.
+        Destroys the object, propagating to all children before the handlers for this object are triggered.
+        This will deactivate the object prior to destruction.
         """
 
         # Propagate the destroy state to all children.
@@ -172,7 +190,7 @@ class GameObject:
         self.deactivate()
         self.__destroyed = True
 
-        for handler in self.__destroyed_handlers:
+        for handler in self.__destroy_handlers:
             handler(self)
 
     @property
@@ -184,9 +202,17 @@ class GameObject:
 
     @property
     def children(self) -> list[Self]:
+        """
+        Returns all the children of this GameObject. If there are no children, an empty list
+        is returned.
+        """
         return self.__children.copy()
 
     def add_child(self, child: Self) -> Self:
+        """
+        Adds a GameObject as a child of this GameObject. If the child object already has a parent an
+        error will be raised. This will set the parent of the child to this GameObject.
+        """
         if not child:
             return self
 
@@ -198,56 +224,25 @@ class GameObject:
         return self
 
     def remove_child(self, child: Self) -> Self:
-        if not child:
+        """
+        Removes a GameObject as a child of this GameObject. If the childs parent is not this GameObject
+         then an error will be raised (an exception to this is if the child has no parent).
+        """
+        # If this child does not have a parent, ignore.
+        if not child or not child.__parent:
             return self
 
         if child.__parent != self:
             raise ValueError("child is not a child of this GameObject")
 
+        child.__parent = None
         self.__children.remove(child)
         return self
 
-    def add_draw_handler(self, handler: Callable[[Self, Any], None]) -> Self:
-        self.__draw_handlers.append(handler) if handler else None
-        return self
-
-    def remove_draw_handler(self, handler: Callable[[Self, Any], None]) -> Self:
-        self.__draw_handlers.remove(handler) if handler else None
-        return self
-
-    def add_update_handler(self, handler: Callable[[Self, float], None]) -> Self:
-        self.__update_handlers.append(handler) if handler else None
-        return self
-
-    def remove_update_handler(self, handler: Callable[[Self, float], None]) -> Self:
-        self.__update_handlers.remove(handler) if handler else None
-        return self
-
-    def add_activate_handler(self, handler: Callable[[Self], None]) -> Self:
-        self.__activate_handlers.append(handler) if handler else None
-        return self
-
-    def remove_activate_handler(self, handler: Callable[[Self], None]) -> Self:
-        self.__activate_handlers.remove(handler) if handler else None
-        return self
-
-    def add_deactivate_handler(self, handler: Callable[[Self], None]) -> Self:
-        self.__deactivate_handlers.append(handler) if handler else None
-        return self
-
-    def remove_deactivate_handler(self, handler: Callable[[Self], None]) -> Self:
-        self.__deactivate_handlers.remove(handler) if handler else None
-        return self
-
-    def add_destroyed_handler(self, handler: Callable[[Self], None]) -> Self:
-        self.__destroyed_handlers.append(handler) if handler else None
-        return self
-
-    def remove_destroyed_handler(self, handler: Callable[[Self], None]) -> Self:
-        self.__destroyed_handlers.remove(handler) if handler else None
-        return self
-
     def draw(self, surface: Any) -> Self:
+        """
+        Draws the GameObject (if `active` and `visible`) and propagates to children (if `active`).
+        """
         if not self.active:
             return self
 
@@ -261,8 +256,19 @@ class GameObject:
         return self
 
     def update(self, dt: float) -> Self:
+        """
+        Updates the GameObject (if `active` and `enabled`) and propagates to children (if `active`).
+        Also removes any destroyed children.
+        """
 
         # Remove any destroyed children.
+        destroyed_children = [
+            child for child in self.__children
+            if child.destroyed
+        ]
+        for child in destroyed_children:
+            child.__parent = None
+
         self.__children = [
             child for child in self.__children
             if not child.destroyed
@@ -278,6 +284,56 @@ class GameObject:
         for child in self.__children:
             child.update(dt)
 
+        return self
+
+    def add_draw_handler(self, handler: Callable[[Self, Any], None]) -> Self:
+        """Adds a `draw` handler."""
+        self.__draw_handlers.append(handler) if handler else None
+        return self
+
+    def remove_draw_handler(self, handler: Callable[[Self, Any], None]) -> Self:
+        """Removes a `draw` handler."""
+        self.__draw_handlers.remove(handler) if handler else None
+        return self
+
+    def add_update_handler(self, handler: Callable[[Self, float], None]) -> Self:
+        """Adds a `update` handler."""
+        self.__update_handlers.append(handler) if handler else None
+        return self
+
+    def remove_update_handler(self, handler: Callable[[Self, float], None]) -> Self:
+        """Removes a `update` handler."""
+        self.__update_handlers.remove(handler) if handler else None
+        return self
+
+    def add_activate_handler(self, handler: Callable[[Self], None]) -> Self:
+        """Adds a `activate` handler."""
+        self.__activate_handlers.append(handler) if handler else None
+        return self
+
+    def remove_activate_handler(self, handler: Callable[[Self], None]) -> Self:
+        """Removes a `activate` handler."""
+        self.__activate_handlers.remove(handler) if handler else None
+        return self
+
+    def add_deactivate_handler(self, handler: Callable[[Self], None]) -> Self:
+        """Adds a `deactivate` handler."""
+        self.__deactivate_handlers.append(handler) if handler else None
+        return self
+
+    def remove_deactivate_handler(self, handler: Callable[[Self], None]) -> Self:
+        """Removes a `deactivate` handler."""
+        self.__deactivate_handlers.remove(handler) if handler else None
+        return self
+
+    def add_destroy_handler(self, handler: Callable[[Self], None]) -> Self:
+        """Adds a `destroy` handler."""
+        self.__destroy_handlers.append(handler) if handler else None
+        return self
+
+    def remove_destroy_handler(self, handler: Callable[[Self], None]) -> Self:
+        """Removes a `destroy` handler."""
+        self.__destroy_handlers.remove(handler) if handler else None
         return self
 
 
